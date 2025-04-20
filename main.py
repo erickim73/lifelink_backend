7095bf304a33dea0f5e75e1cf02da0f4a70c2c8d
erickim73 <seyoon2006@gmail.com>
2025-04-20 17:24:09 -0500

    structured instructions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-model_path = "./mistral-7b-q4_0.gguf"
+model_path = "./mistral-7b-q4km.gguf"
 
 model = Llama(model_path=model_path, n_ctx=2048, n_threads=8, n_batch=8, temperature=0.7, top_p=0.95, repeat_penalty=1.2, verbose=False)
 
@@ -13,12 +13,17 @@ model = Llama(model_path=model_path, n_ctx=2048, n_threads=8, n_batch=8, tempera
 @app.route('/chat', methods=['POST'])
 def generate_reply():
     data = request.get_json()
-    prompt = data.get('prompt', '')
+    prompt_text = data.get('prompt', '')
+    prompt = (
+        "<>[INST] You are a helpful medical assistant. "
+        "Answer the user's question based on the provided context if any. "
+        f"### Question: {prompt_text} [/INST] "
+    )
     
-    output = model(prompt=prompt, max_tokens=128)
+    output = model(prompt=prompt, max_tokens=512)
     return jsonify({
         "response": output['choices'][0]['text']
     })
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
